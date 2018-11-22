f09bd617bfbf0c0f9ed4c7208334b0be66a0c497
Steven Chen <steven.chen4hao@gmail.com>
2018-11-22 09:37:54 +0800

    install simple_form, devise and add name, title to devise user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    #註冊頁面
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :title])
+    #修改註冊資料
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :title])
+  end
+
 end
---
+++ db/migrate/20181121085013_add_name_title_to_user.rb
@@ -0,0 +1,6 @@
+class AddNameTitleToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :title, :string
+  end
+end
